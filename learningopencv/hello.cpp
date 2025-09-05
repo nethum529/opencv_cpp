ae9f7c070f5f1173ceb77b8d88bb4b1e06f846d2
nethum529 <nethumweerasinghe@icloud.com>
2025-09-05 10:21:23 -0500

    Made CMakeLists.txt

---
+++ learningopencv/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main() {
+
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
