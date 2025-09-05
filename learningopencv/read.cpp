c1fec09328cc05b8dfe6e8495f47778de0ce8f95
nethum529 <nethumweerasinghe@icloud.com>
2025-09-05 12:49:00 -0500

    Finishing with importing images, videos and webcams

--- learningopencv/read.cpp
+++ learningopencv/read.cpp
@@ -7,13 +7,53 @@ using namespace std;
 using namespace cv;
 
 // Importing Images //
+// int main() {
+
+//     string path = "Resources/test.png";
+//     Mat img = imread(path);
+//     imshow("Image", img);
+//     waitKey(0); // won't close until the close button is pressed
+
+
+
+//     return 0;
+
+// }
+
+
+
+// Importing Videos //'
+// int main() {
+
+//     string path = "Resources/test_video.mp4";
+//     VideoCapture cap(path);
+//     Mat img;
+//     while(true) {
+
+//     cap.read(img);
+
+//     imshow("Image", img);
+//     waitKey(1); // won't close until the close button is pressed
+//     };
+
+
+//     return 0;
+
+// }
+
+
+// Webcam //
 int main() {
 
-    string path = "Resources/test.png";
-    Mat img = imread(path);
-    imshow("Image", img);
-    waitKey(0); // won't close until the close button is pressed
+    VideoCapture cap(0);
+    Mat img;
+        while(true) {
 
+        cap.read(img);
+
+        imshow("Image", img);
+        waitKey(1); // won't close until the close button is pressed
+    };
 
 
     return 0;
