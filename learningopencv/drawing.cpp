1bf6adc813f1e9247d1c030606b4faf2c7137493
nethum529 <nethumweerasinghe@icloud.com>
2025-09-05 20:34:14 -0500

    Finished drawing.cpp

--- learningopencv/drawing.cpp
+++ learningopencv/drawing.cpp
@@ -8,7 +8,15 @@ using namespace std;
 // Drawing Shapes and Text //
 int main() {
 
-    Mat img(512, 512, CV_8UC3, Scalar(255, 0, 255));
+    // blank image
+    Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
+
+    circle(img, Point(256, 256), 155, Scalar(126, 78, 45), FILLED);
+    rectangle(img, Point(130, 226), Point(382, 286), Scalar(68, 128, 201), FILLED);
+    line(img, Point(130, 296), Point(382, 296), Scalar(123, 198, 68), 3);
+
+
+    putText(img, "Nethum's Workshop", Point(137, 262), FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 69, 255));
     imshow("Image", img);
     waitKey(0);
     return 0;
